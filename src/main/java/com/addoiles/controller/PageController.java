9f4221a7c0cff4162eeb345fd1ca37adf88e1ef1
yangrunkang <1743703238@qq.com>
2017-07-19 15:40:38 +0800

    qwerqwerqwer

--- src/main/java/com/addoiles/controller/PageController.java
+++ src/main/java/com/addoiles/controller/PageController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class IndexController {
+public class PageController {
 
     private static final String HOME_PAGE = "home";
     private static final String ADDOIL_PAGE = "addoil";
