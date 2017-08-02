ea12957d3513ff33e7c47711def88582e3fd5877
yangrunkang <1743703238@qq.com>
2017-08-02 17:03:46 +0800

    Mod: Code Optimalize

--- src/main/webapp/js/oil_experence.js
+++ src/main/webapp/js/oil_experence.js
@@ -1,8 +1,14 @@
 $(function() {
 	$("#saveContent").click(function(){
-		console.log("Experence点击保存按钮")
+		console.log("Experence点击保存按钮");
+		var content = $("textarea[name='content']").val();
+		console.log($(editor.textarea.element.innerHTML));
+		console.log($(editor.textareaElement).val());
+		console.log($(editor.textareaElement));
+		console.log($(editor.textarea));
 	});
 	$("#saveTempContent").click(function(){
-		console.log("Experence点击临时保存按钮")
+		console.log("Experence点击临时保存按钮");
 	});
-});  
+	
+});
