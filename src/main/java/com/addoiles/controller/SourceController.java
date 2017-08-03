320eaa3ec5587d51cbde27971ef59a75a6ce3d18
yangrunkang <1743703238@qq.com>
2017-08-03 17:59:12 +0800

    Mod(Codes): 删除cfeditor相关代码

--- src/main/java/com/addoiles/controller/SourceController.java
+++ src/main/java/com/addoiles/controller/SourceController.java
@@ -1,8 +1,6 @@
 package com.addoiles.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Description: 资源Controller,用于获取各种资源
@@ -11,16 +9,6 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 public class SourceController {
-
-    /**
-     * 获取编辑器
-     * @param pageName 来源页面
-     * @return
-     */
-    @RequestMapping("getCfeditor")
-    public String getCfeditor(String pageName, ModelAndView modelAndView) {
-        return "components/cfeditor/cfeditor";
-    }
 
 
 }
