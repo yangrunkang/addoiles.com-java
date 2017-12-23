f14481629f305e3b575b1a3e0ef52bd62727d03e
yangrunkang <1743703238@qq.com>
2017-12-23 10:10:12 +0800

    Add: 整理DB字段枚举

--- src/main/java/com/addoiles/common/enums/DBFieldEnum.java
+++ src/main/java/com/addoiles/common/enums/DBFieldEnum.java
@@ -30,8 +30,7 @@ public class DBFieldEnum {
         /**
          * IT技术沉淀
          */
-        IT_TECH(2),
-        ;
+        IT_TECH(2),;
 
         private final int value;
 
@@ -63,8 +62,7 @@ public class DBFieldEnum {
         /**
          * 草稿
          */
-        DRAFT(2),
-        ;
+        DRAFT(2),;
 
         private final int value;
 
@@ -91,9 +89,7 @@ public class DBFieldEnum {
         /**
          * 删除
          */
-        DELETED(1),
-
-        ;
+        DELETED(1),;
 
         private final int value;
 
@@ -119,8 +115,7 @@ public class DBFieldEnum {
         /**
          * 梦想
          */
-        DREAMS(1),
-        ;
+        DREAMS(1),;
 
         private final int value;
 
@@ -146,13 +141,90 @@ public class DBFieldEnum {
         /**
          * 删除
          */
-        DELETED(1),
-
-        ;
+        DELETED(1),;
 
         private final int value;
 
         MicroContentDeleteStatusEnum(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    /**
+     * 导航栏是否启用枚举
+     */
+    public enum NavSettingsIsEnableEnum {
+
+        /**
+         * 启用
+         */
+        ENABLE(0),
+
+        /**
+         * 不启用
+         */
+        NO_ENABLE(1),;
+
+        private final int value;
+
+        NavSettingsIsEnableEnum(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    /**
+     * 问题删除状态枚举
+     */
+    public enum QuestionDeletedStatusEnum {
+
+        /**
+         * 正常
+         */
+        NORMAL(0),
+
+        /**
+         * 删除
+         */
+        DELETED(1),;
+
+        private final int value;
+
+        QuestionDeletedStatusEnum(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+
+    /**
+     * 用户删除状态枚举
+     */
+    public enum UserDeletedStatusEnum {
+
+        /**
+         * 正常
+         */
+        NORMAL(0),
+
+        /**
+         * 删除
+         */
+        DELETED(1),;
+
+        private final int value;
+
+        UserDeletedStatusEnum(int value) {
             this.value = value;
         }
 
