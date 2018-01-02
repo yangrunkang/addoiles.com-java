3361214857e48ccddee166d3230976981bc55cba
yangrunkang <1743703238@qq.com>
2018-01-02 11:07:02 +0800

    Mod: 上线注意事项; 添加Redis开发Key

--- src/main/java/com/addoiles/db/redis/OilRedisConstant.java
+++ src/main/java/com/addoiles/db/redis/OilRedisConstant.java
@@ -12,10 +12,20 @@ package com.addoiles.db.redis;
 
 public class OilRedisConstant {
 
+    /**
+     * 开发时加上
+     * 生产是去掉
+     */
+    private static final String REDIS_ENV = "DEV";
 
-    private static String OIL_WEBSITE = "com.addoiles.wwww";
+    /**
+     * redis key 前缀
+     */
+    private static String OIL_WEBSITE = REDIS_ENV +"com.addoiles.wwww";
 
-    public static final String USERS_ID_NAME_LIST = OIL_WEBSITE + "users.ids.names.list";
-
+    /**
+     * 用户ID-用户名 映射集合
+     */
+    public static String USERS_ID_NAME_LIST = OIL_WEBSITE + "users.ids.names.list";
 
 }
