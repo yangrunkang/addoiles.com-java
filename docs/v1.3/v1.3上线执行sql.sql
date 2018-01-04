1e05dda2ed423cf57a3f6a5237375ae7095597f3
yangrunkang <1743703238@qq.com>
2018-01-04 17:11:45 +0800

    Mod: v1.3上线执行sql question 添加 类型字段

--- docs/v1.3/v1.3上线执行sql.sql
+++ docs/v1.3/v1.3上线执行sql.sql
@@ -1,5 +1,6 @@
 -- 导航栏表 增添问答区导航栏数据
-INSERT INTO `addoiles`.`nav_settings` (`nav_name`, `nav_router`, `nav_icon`, `is_start`) VALUES ('问答区', '/QAArea', 'cube', '0');
+INSERT INTO `addoiles`.`nav_settings` (`nav_name`, `nav_router`, `nav_icon`, `is_start`) VALUES ('论坛', '/OilForm',
+                                                                                                 'cube', '0');
 -- 导航栏表 添加排序字段
 alter table `addoiles`.`nav_settings` add `sort` int(2) DEFAULT 0 COMMENT '排序字段 1 最大,排在前面' AFTER `is_start`;
 -- 更新导航栏排序数据
@@ -8,12 +9,13 @@ when '主页' then 0
 when '梦想墙' then 1
 when '经历分享' then 2
 when 'IT技术' then 3
-when '问答区' then 4
+when '论坛' then 4
 when '聊天机器人' then 5
 when '登录' then 6
 when '注册' then 7
 when '软件评测' then 90
 when '程序员的生活记录' then 91
 end;
-
+-- 问题表 添加问题类型字段
+alter table `addoiles`.`question` add `type` int(2) DEFAULT 0 COMMENT '问题类型 0-编程语言 1-开发问题 2-缓存技术 3-操作系统 4-学习' AFTER `content`;
 
