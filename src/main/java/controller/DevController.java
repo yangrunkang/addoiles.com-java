0e185db59059a597d4f1ea1214a726cafe56bb08
yangrunkang <1743703238@qq.com>
2018-01-06 09:10:48 +0800

    Add: 添加DevController,用于管理开发数据

---
+++ src/main/java/controller/DevController.java
@@ -0,0 +1,43 @@
+package controller;
+
+import com.addoiles.db.redis.OilRedisConstant;
+import com.addoiles.db.redis.inter.RedisService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+
+/**
+ * Description: Dev 数据清理
+ * All rights Reserved, Designed By HQYG
+ * @apiNote 统一访问地址: http://ip:port/project_name/cleanRedisDevData
+ * @author Yangrunkang
+ * @Copyright: Copyright(C) 2016
+ * @Company: HQYG
+ * @CreateDate: 2018/1/6 8:58
+ */
+@Controller
+public class DevController {
+
+    private static Logger logger = LoggerFactory.getLogger(DevController.class);
+
+    @Resource
+    private RedisService redisService;
+
+    /**
+     *  清除开发Redis Key
+     * @return
+     */
+    @RequestMapping(value = "cleanRedisDevData",method = RequestMethod.GET)
+    @ResponseBody
+    public Object cleanRedisDevData(){
+        redisService.deleteKeys(OilRedisConstant.OIL_WEBSITE + "*");
+        logger.info("------>dev redis data cleaned");
+        return "dev redis data cleaned";
+    }
+
+}
