2baf5b8d4d996de13e58782466747cfddd4bffaf
yangrunkang <yangrunkang53@gmail.com>
2019-06-17 14:08:25 +0800

    Fix: 修复图片上传异常bug
    - 使用alibaba的fastjson解析失败,改用使用gson,google的

--- src/main/java/com/addoiles/util/JsonUtils.java
+++ src/main/java/com/addoiles/util/JsonUtils.java
@@ -1,45 +1,29 @@
 package com.addoiles.util;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.TypeReference;
-import com.alibaba.fastjson.parser.Feature;
-import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.google.gson.Gson;
 import org.apache.commons.lang3.StringUtils;
 
-import static com.alibaba.fastjson.serializer.SerializerFeature.*;
-
+/**
+ * 使用Gson 替换alibaba的fastjson
+ */
 public class JsonUtils {
+
+    private static Gson gson = new Gson();
 
     private JsonUtils() {
     }
 
     public static String toJson(Object object) {
-        return JSON.toJSONString(object,
-                new SerializerFeature[]{WriteMapNullValue,
-                        DisableCircularReferenceDetect,
-                        WriteNonStringKeyAsString});
+        return gson.toJson(object);
     }
 
-    public static String toJsonFormat(Object object) {
-        return JSON.toJSONString(object,
-                new SerializerFeature[]{WriteMapNullValue, PrettyFormat,
-                        DisableCircularReferenceDetect,
-                        WriteNonStringKeyAsString});
-    }
-
-    public static Object toJsonObject(Object obj) {
-        return JSON.toJSON(obj);
-    }
 
     public static <T> T fromJson(String jsonString, Class<T> clazz) {
         if (StringUtils.isEmpty(jsonString)) {
             return null;
         }
-        return JSON.parseObject(jsonString, clazz);
+        return gson.fromJson(jsonString,clazz);
     }
 
-    public static <T> T fromJson(String jsonString) throws Exception {
-        return JSON.parseObject(jsonString, new TypeReference<T>() {
-        }, new Feature[0]);
-    }
+
 }
